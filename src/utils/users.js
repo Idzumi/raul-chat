b785f2a6032759b06d159b31c9437d5bb7e200b1
Maryia Hancharevich <maryiahancharevich@maryias-macbook-pro.home>
2023-08-27 11:30:04 +0200

    Init commit

---
+++ src/utils/users.js
@@ -0,0 +1,86 @@
+const users = []
+
+// addUser, removeUser, getUser, getUsersInRoom
+
+const addUser = ({ id, username, room }) => {
+    // Clean the data
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    // Validate the data
+    if (!username || !room) {
+        return {
+            error: 'Username and room are required!'
+        }
+    }
+
+    // Check for exisiting user
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username === username
+    })
+
+    // Validate username
+    if (existingUser) {
+        return {
+            error: 'Username is in use!'
+        }
+    }
+
+    // Store user
+    const user = { id, username, room }
+    users.push(user)
+    return { user }
+
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => {
+        return user.id === id
+    })
+
+    if (index != -1) {
+        return users.splice(index, 1)[0]
+    }
+}
+
+const getUser = (id) => {
+    return users.find((user) => user.id === id)
+}
+
+const getUsersInRoom = (room) => {
+    room = room.trim().toLowerCase()
+    return roomUsers = users.filter((user) => user.room === room
+)
+     
+}
+
+addUser(
+    {
+        id: 22,
+        username: 'Andrew',
+        room: 'South Philly'
+    }
+)
+
+addUser(
+    {
+        id: 42,
+        username: 'Mike',
+        room: 'South Philly'
+    }
+)
+
+addUser(
+    {
+        id: 32,
+        username: 'Andrew',
+        room: 'Center City'
+    }
+)
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+}
